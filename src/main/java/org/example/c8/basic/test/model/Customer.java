0034c3d5f4bd004faa325681876ad5f5b3016d86
daniel-ewing <daniel.ewing@camunda.com>
2023-06-26 14:20:28 -0400

    Initial Java Zeebe test.

--- src/main/java/org/example/c8/basic/test/model/Customer.java
+++
@@ -1,9 +0,0 @@
-package org.example.c8.basic.test.model;
-
-public class Customer {
-    private static final long serialVersionUID = 1L;
-    private String firstName;
-    private String lastName;
-    private Address address;
-
-}
