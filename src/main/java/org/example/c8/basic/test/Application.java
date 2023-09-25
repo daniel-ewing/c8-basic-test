e314ea297abd125f79892d1bc5aaaf96e1942e9b
daniel-ewing <daniel.ewing@camunda.com>
2023-09-25 12:50:12 -0400

    Demonstrate calling newCancelInstanceCommand with invalid id does not generate any error.

--- src/main/java/org/example/c8/basic/test/Application.java
+++ src/main/java/org/example/c8/basic/test/Application.java
@@ -1,6 +1,7 @@
 package org.example.c8.basic.test;
 
 import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.CancelProcessInstanceResponse;
 import io.camunda.zeebe.client.api.response.PublishMessageResponse;
 
 import java.util.Map;
@@ -10,8 +11,8 @@ public class Application {
 
     public static void main(final String... args) {
         try (ZeebeClient client = ZeebeClient.newClientBuilder()
-                .caCertificatePath("/home/administrator/software/camunda/camunda-platform-8/certificates/dev.c8.separated.crt")
-                .gatewayAddress("127.0.0.1:443")
+//                .caCertificatePath("/home/administrator/software/camunda/camunda-platform-8/certificates/dev.c8.separated.crt")
+                .gatewayAddress("127.0.0.1:26500")
                 .usePlaintext()
                 .build()) {
             client.newTopologyRequest().send().join();
@@ -23,6 +24,9 @@ public class Application {
                     .variables(Map.of("var1", "val1"))
                     .send()
                     .join();
+
+            CancelProcessInstanceResponse cancelProcessInstanceResponse = client.newCancelInstanceCommand(1L).send().join();
+
 
         }
     }
